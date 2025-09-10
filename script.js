3bbd38cd2dc7438d8a0445e97f176747cc19a9ff
albert <fung.albert@gmail.com>
2025-09-10 08:11:10 -0400

    Major update v.13 - color changes, logo changes, also added in GE category.

--- script.js
+++ script.js
@@ -78,12 +78,36 @@ function initAllData() {
 
           });
         }
+        else if (sheetName === "Gastroesophageal") {
+          rows.forEach((row, i) => {
+            gastroesophageal_master["names"].push(row["Trial Name"]);
+            gastroesophageal_master["setting"].push(row["Disease Setting"]);
+            gastroesophageal_master["fullTitle"].push(row["Full title"]);
+            gastroesophageal_master["additionalNotes"].push(row["Additional notes"]);
+            gastroesophageal_master["tissueRequirements"].push(row["Tissue Requirements"]);
+            gastroesophageal_master["arms"].push(row["Trial Intervention/Arms"]);
+            gastroesophageal_master["keyCriteria"].push(row["Eligibility "]);
+            gastroesophageal_master["contact"].push(row["Contacts"]);
+            gastroesophageal_master["NCT"].push(row["NCT number"]);
+            //adding in base64 encoded images
+            gastroesophageal_master["schema"].push(row["Schema image data"]);
+
+            if (row["Biomarker"])
+            {
+             biomarker_master["categories"].push("PDAC");
+             biomarker_master["names"].push(row["Trial Name"]);
+             biomarker_master["biomarker"].push(row["Biomarker"]);
+             biomarker_master["keyInCategory"].push(i);
+            }
+
+          });
+        }
       }
         for (const[sheetName,rows] of Object.entries(data.Changes))
         {
           rows.forEach(row => {
           changes.push([sheetName, row["Trial Name"], row.timestamp]);
-  });
+        });
         }
     })
     //.catch(err => console.error('Error parsing JSON:', err));
@@ -101,6 +125,20 @@ const biomarker_master ={
   names: [],
   biomarker: []
 }
+
+const gastroesophageal_master = {
+  names: [],
+  setting: [],
+  fullTitle: [],
+  additionalNotes: [],
+  tissueRequirements: [],
+  arms: [],
+  keyCriteria: [],
+  contact: [],
+  NCT:[],
+  schema:[],
+  label: "gastroesophageal" //this is the sheet name
+};
 
 const biliary_CCA_master = {
   names: [],
@@ -179,6 +217,7 @@ function fillTrialNameBasedOnBiomarker(biomarker)
       outputHTMLline += '" autocomplete="off">';
 
       //this is the label for the button
+      
       outputHTMLline += '<label class="btn btn-outline-dark text-start" for="';
       outputHTMLline += biomarker_master[`categories`][i] + "_" + biomarker_master[`keyInCategory`][i];
       outputHTMLline += '">';
@@ -228,7 +267,29 @@ function fillTrialNameBasedOnSetting(mainCategory, setting) {
   var outputHTMLstring = "";
 
   //create vertical radio button group
-  outputHTMLstring += '<div class="btn-group-vertical p-3" role="group">';
+  outputHTMLstring += '<div class="btn-group-vertical p-3" role="group" ';
+
+  switch (mainCategory) {
+    case gastroesophageal_master:
+      outputHTMLstring += 'data-bs-theme="ge">';
+      break;
+  
+    case liver_HCC_master:
+      outputHTMLstring += 'data-bs-theme="hcc">';
+      break;
+    
+    case pancreas_master:
+      outputHTMLstring += 'data-bs-theme="pdac">';
+      break;
+    
+    case biliary_CCA_master:
+      outputHTMLstring += 'data-bs-theme="cca">';
+      break;
+
+    default:
+      outputHTMLstring += '>';
+      break;
+  }
 
   for (i = 0; i < totalTrialsAvailable; i++) {
     //list all
@@ -258,7 +319,7 @@ function fillTrialNameBasedOnSetting(mainCategory, setting) {
       outputHTMLstring += '" autocomplete="off">';
 
       //this is the label for the button
-      outputHTMLstring += '<label class="btn btn-outline-dark text-start" for="trialName';
+      outputHTMLstring += '<label class="btn btn-outline-custom text-start" for="trialName';
       outputHTMLstring += [i];
       outputHTMLstring += '">';
       outputHTMLstring += mainCategory[`names`][i];
@@ -301,10 +362,32 @@ function fillTrialDetails(mainCategory, key) {
   var NCTstring = "";
 
   //ul elements
-  htmlString += '<ul class="list-group list-group p-3">';
+  htmlString += '<ul class="list-group list-group p-3"';
+
+  switch (mainCategory) {
+    case gastroesophageal_master:
+      htmlString += ' data-bs-theme="ge">';
+      break;
+
+    case liver_HCC_master:
+      htmlString += ' data-bs-theme="hcc">';
+      break;
+
+    case pancreas_master:
+      htmlString += ' data-bs-theme="pdac">';
+      break;
+
+    case biliary_CCA_master:
+      htmlString += ' data-bs-theme="cca">';
+      break;
+  
+    default:
+      htmlString += '>';
+      break;
+  }
 
   //Full title
-  titleString += '<li class="list-group-item bg-gray-200 text-gray-900 btn btn-toggle d-inline-flex align-items-center fw-semibold" data-bs-toggle="collapse" data-bs-target="#fullTitleCollapse">Full title</li><li class="list-inline-item ps-5 collapse" id="fullTitleCollapse"><p class="py-2">';
+  titleString += '<li class="detailsSection01 list-group-item btn btn-toggle d-inline-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#fullTitleCollapse">Full title</li><li class="list-inline-item ps-5 collapse" id="fullTitleCollapse"><p class="py-2">';
   titleString += mainCategory[`fullTitle`][key];
   titleString += '</p></li>';
   
@@ -312,38 +395,15 @@ function fillTrialDetails(mainCategory, key) {
   //Additional notes (optional)
   if(mainCategory[`additionalNotes`][key] != null)
     {
-    notesString += '<li class="list-group-item bg-gray-200 text-gray-900 btn btn-toggle d-inline-flex align-items-center fw-semibold" data-bs-toggle="collapse" data-bs-target="#notesCollapse">Summary</li><li class="list-inline-item ps-5 collapse" id="notesCollapse"><p class="py-2">';
+    notesString += '<li class="detailsSection01 list-group-item btn btn-toggle d-inline-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#notesCollapse">Summary</li><li class="list-inline-item ps-5 collapse" id="notesCollapse"><p class="py-2">';
     notesString += mainCategory[`additionalNotes`][key].replaceAll("\n", "<br/>");
     notesString += '</p></li>';
     }
  
-  
-  //Tissue requirements (optional)
-  if(mainCategory[`tissueRequirements`][key] != null)
-    {
-    tissueReqString += '<li class="list-group-item bg-gray-400 text-gray-900 btn btn-toggle d-inline-flex align-items-center fw-semibold" data-bs-toggle="collapse" data-bs-target="#tissueReqCollapse">Tissue requirements</li><li class="list-inline-item ps-5 collapse" id="tissueReqCollapse"><p class="py-2">';
-    tissueReqString += mainCategory[`tissueRequirements`][key].replaceAll("\n", "<br/>");
-    tissueReqString += '</p></li>';
-    }
-  
-  
-  //Arms
-  armString += '<li class="list-group-item bg-gray-300 text-gray-900 btn btn-toggle d-inline-flex align-items-center fw-semibold" data-bs-toggle="collapse" data-bs-target="#armsCollapse">Arms/Cohorts</li><li class="list-inline-item ps-5 collapse" id="armsCollapse"><p class="py-2">';
-  armString += mainCategory[`arms`][key].replaceAll("\n", "<br/>");
-  armString += "</p></li>";
-
-  //Key criteria
-  if(mainCategory[`keyCriteria`][key] != null)
-    {
-  criteriaString += '<li class="list-group-item bg-gray-400 text-gray-900 btn btn-toggle d-inline-flex align-items-center fw-semibold" data-bs-toggle="collapse" data-bs-target="#keyCollapse">Key criteria</li><li class="list-inline-item ps-5 collapse" id="keyCollapse"><p class="py-2">';
-  criteriaString += mainCategory[`keyCriteria`][key].replaceAll("\n", "<br/>");      
-  criteriaString += "</p></li>";
-    }
-  
   //Schema
   if(mainCategory[`schema`][key] != null)
     {
-  schemaString += '<li class="list-group-item bg-gray-300 text-gray-900 btn btn-toggle d-inline-flex align-items-center fw-semibold" data-bs-toggle="collapse" data-bs-target="#schemaCollapse">Schema</li><li class="list-inline-item ps-5 collapse" id="schemaCollapse"><p class="my-2">';
+  schemaString += '<li class="detailsSection02 list-group-item btn btn-toggle d-inline-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#schemaCollapse">Schema</li><li class="list-inline-item ps-5 collapse" id="schemaCollapse"><p class="my-2">';
   // schemaString += '<a href="" onclick="event.preventDefault(); openImage(`';
   // schemaString += mainCategory[`schema`][key]; 
   // schemaString += '`)">View Schema</a></p></li>';
@@ -351,9 +411,32 @@ function fillTrialDetails(mainCategory, key) {
   schemaString += mainCategory[`schema`][key];
   schemaString += "'></p>";
     }
+  
+  //Arms
+  armString += '<li class="detailsSection02 list-group-item btn btn-toggle d-inline-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#armsCollapse">Arms/Cohorts</li><li class="list-inline-item ps-5 collapse" id="armsCollapse"><p class="py-2">';
+  armString += mainCategory[`arms`][key].replaceAll("\n", "<br/>");
+  armString += "</p></li>";
+    
+  //Tissue requirements (optional)
+  if(mainCategory[`tissueRequirements`][key] != null)
+    {
+    tissueReqString += '<li class="detailsSection03 list-group-item btn btn-toggle d-inline-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#tissueReqCollapse">Tissue requirements</li><li class="list-inline-item ps-5 collapse" id="tissueReqCollapse"><p class="py-2">';
+    tissueReqString += mainCategory[`tissueRequirements`][key].replaceAll("\n", "<br/>");
+    tissueReqString += '</p></li>';
+    }
+
+  //Key criteria
+  if(mainCategory[`keyCriteria`][key] != null)
+    {
+  criteriaString += '<li class="detailsSection03 list-group-item btn btn-toggle d-inline-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#keyCollapse">Key criteria</li><li class="list-inline-item ps-5 collapse" id="keyCollapse"><p class="py-2">';
+  criteriaString += mainCategory[`keyCriteria`][key].replaceAll("\n", "<br/>");      
+  criteriaString += "</p></li>";
+    }
+  
+  
 
   //Contact
-  contactString += '<li class="list-group-item bg-gray-500 text-gray-900 btn btn-toggle d-inline-flex align-items-center fw-semibold" data-bs-toggle="collapse" data-bs-target="#contactCollapse">Contact</li><li class="list-inline-item ps-5 collapse" id="contactCollapse"><p class="py-2">';
+  contactString += '<li class="detailsSection04 list-group-item btn btn-toggle d-inline-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#contactCollapse">Contact</li><li class="list-inline-item ps-5 collapse" id="contactCollapse"><p class="py-2">';
 
   
   //contactString += mainCategory[`contact`][key];
@@ -364,7 +447,7 @@ function fillTrialDetails(mainCategory, key) {
   //More information link
   if(mainCategory[`NCT`][key] != null)
     {
-  NCTstring += '<li class="list-group-item bg-gray-600 text-gray-900 btn btn-toggle d-inline-flex align-items-center fw-semibold" data-bs-toggle="collapse" data-bs-target="#nctCollapse">Helpful links</li><li class="list-inline-item ps-5 collapse" id="nctCollapse"><p class="py-2">';
+  NCTstring += '<li class="detailsSection04 list-group-item btn btn-toggle d-inline-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#nctCollapse">Helpful links</li><li class="list-inline-item ps-5 collapse" id="nctCollapse"><p class="py-2">';
   NCTstring += '<a target="_blank" href="https://clinicaltrials.gov/study/';
   NCTstring += mainCategory[`NCT`][key];
   NCTstring += '">Link to clinicaltrials.gov</p></li>';
@@ -764,6 +847,83 @@ doc.autoTable({
         doc.setFontSize(15);
         doc.setFont("helvetica", "bold");
         doc.text("CCA", centerX, centerY, { angle: 90, align: 'center' });
+      }
+      return false; // skip drawing this cell
+    }
+  }
+});
+ 
+}
+if (data.Gastroesophageal != null){
+// Map of PDF header → JSON key
+const columnMap = [
+  { header: "", key: null }, // first column for vertical rowspan text
+  { header: "Title", key: "Trial Name" },
+  { header: "Setting", key: "Disease Setting" },
+  { header: "Arms", key: "Trial Intervention/Arms" },
+  { header: "Key Criteria", key: "Eligibility " },
+  { header: "Contact", key: "Contacts" }
+];
+
+// Build headers from mapping
+const headers = columnMap.map(col => col.header);
+
+  const GErows = data.Gastroesophageal.map(trial =>
+  columnMap.map((col, colIndex) => {
+    return trial[col.key] ?? "";
+  })
+);
+
+// Variable to store rowspan height
+let groupRowSpanHeight = 0;
+
+doc.addPage();
+doc.autoTable({
+  head: [headers],
+  body: GErows,
+  styles: { fontSize: 10, cellPadding: 2, overflow: 'linebreak', lineWidth: 0.2, lineColor: [50, 50, 50]},
+  headStyles: { fillColor: [153, 153, 225], textColor: 255 },
+  columnStyles: {
+    0: { cellWidth: 12 },
+    1: { cellWidth: 80 },
+    2: { cellWidth: 30 },
+    3: { cellWidth: 55 },
+    4: { cellWidth: 55 },
+    5: { cellWidth: 40 }
+  },
+  didParseCell: function (data) { 
+    if (data.section === 'body') {
+      if (data.row.index % 2 === 0) {
+        data.cell.styles.fillColor = [200, 200, 239];
+      } else {
+        data.cell.styles.fillColor = [255, 255, 255];
+      }
+    }
+  },
+  willDrawCell: function(data) {
+    // Skip all cells in the first column (header + body)
+    if (data.column.index === 0) {
+      if (data.row.index === 0 && data.section === 'body') {
+        const table = data.table;
+        
+        // Calculate total height including header + body
+        const totalHeight = table.head.reduce((sum, row) => sum + row.height, 0)
+                          + table.body.reduce((sum, row) => sum + row.height, 0);
+        const x = data.cell.x;
+        const y = table.head[0].cells[0].y; // start at top of header
+        const width = data.cell.width -1;
+
+        // Draw the filled rectangle
+        doc.setFillColor(153, 153, 225);
+        doc.rect(x, y, width, totalHeight, 'F');
+
+        // Draw rotated text, centered vertically + horizontally
+        const centerX = x + width / 2 + 25;
+        const centerY = y + totalHeight / 2 + 20;
+        doc.setTextColor(255, 255, 255);
+        doc.setFontSize(15);
+        doc.setFont("helvetica", "bold");
+        doc.text("Gastroesophageal", centerX, centerY, { angle: 90, align: 'center' });
       }
       return false; // skip drawing this cell
     }
